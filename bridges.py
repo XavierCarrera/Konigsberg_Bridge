45b53e7d3d0606f02b57a17619c5cd0e0db47e95
Xavier Carrera <xavier.carrera1990@gmail.com>
2020-08-05 21:16:00 -0500

    Second model

--- bridges.py
+++ bridges.py
@@ -2,11 +2,11 @@
 import matplotlib.pyplot as plt
 nx.draw(P, with_labels = True)
 
-# Initiating an empty Graph object 
-P = nx.Graph() # create an empty object
-# You can add nodes using add_nodes_from()
+
+P = nx.Graph() 
+
 P.add_nodes_from(['A','B','C','D', 'E'])
-# Use add_edges_from to add pairwise relationships
+
 P.add_edges_from ([('B','C'), ('A','C'), ('B','D'), ('D','A'), ('D','E'), ('B','E')])
 
 print(P.nodes())
